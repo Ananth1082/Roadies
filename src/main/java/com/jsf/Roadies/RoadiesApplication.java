b20472567516ebb353c34410d042318f09e7b388
Ananth1082 <ananthshetty203@gmail.com>
2024-11-05 17:28:58 +0530

    feat: Project Setup

---
+++ src/main/java/com/jsf/Roadies/RoadiesApplication.java
@@ -0,0 +1,13 @@
+package com.jsf.Roadies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsf/Roadies/RoadiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsf.Roadies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
